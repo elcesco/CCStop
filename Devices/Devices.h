fdc0d86bd0c48c1534f146b794b311fd0b1739c7
francesco.sole@sole-online.de <francesco@ubuntu>
2016-10-30 13:42:47 -0700

    added uber class for devices

---
+++ Devices/Devices.h
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   Devices.h
+ * Author: francesco
+ *
+ * Created on October 30, 2016, 1:40 PM
+ */
+
+#ifndef DEVICES_H
+#define DEVICES_H
+
+class Devices {
+public:
+    Devices();
+    virtual ~Devices();
+private:
+
+};
+
+#endif /* DEVICES_H */
+
