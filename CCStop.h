aad468783ad8a54f6eb023080aeb00cfa9b27431
francesco.sole@sole-online.de <francesco@ubuntu>
2016-10-30 13:26:19 -0700

    initial commit

---
+++ CCStop.h
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   CCStop.h
+ * Author: francesco
+ *
+ * Created on October 30, 2016, 11:43 AM
+ */
+
+#ifndef CCSTOP_H
+#define CCSTOP_H
+
+#include <cstdlib>
+#include <iostream>
+#include <memory>
+
+class CCStop {
+public:
+    CCStop();
+    virtual ~CCStop();
+private:
+
+};
+
+#endif /* CCSTOP_H */
+
